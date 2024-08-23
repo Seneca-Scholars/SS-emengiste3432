4c6e7ae30ddd502de1be87f78963aebd6693caac
Emanuel Mengiste <emanuelmengiste@Emanuels-MacBook-Pro.local>
2024-08-23 15:04:26 -0500

    Updated App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
+
+ const [info, setInfo] = useState([]);
+  useEffect(() => {
+    fetch("/api/items")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network does not work")
+      }
+      return response.json();
+    })
+    .then(info => {
+      setInfo(info);
+    })
+
+    .catch(error => {
+      console.error("Error fetching data, ", error);
+      }
+  )},
+  []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {info.map((data) => {
+            return (
+              <tr>
+                <td>{data.id}</td>
+                <td>{data.data}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-}
+   }
 
 export default App;
